8ca8194241d78b194030b163d2790adbd3daacd7
Hasan <Hasan@10.44.104.22>
2022-12-10 16:46:41 +0300

    commit

---
+++ src/raceptDemo1/ab.java
@@ -0,0 +1,23 @@
+package raceptDemo1;
+
+public class ab {
+
+	public static void main(String[] args) {
+	int sayi1 = 99;
+	int sayi2 = 55;
+	int sayi3 = 150;
+	int enBuyuk = sayi1;
+	
+	if(enBuyuk<sayi2) {
+	enBuyuk = sayi2;	
+	}
+	if(enBuyuk<sayi3) {
+		enBuyuk = sayi3;
+	}
+		System.out.println("en büyük"+ enBuyuk);
+		
+	
+
+	}
+
+}
